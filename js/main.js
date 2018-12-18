20e23ab2e91248b67ee67bc2dfa06015cd3c8ce3
JamieJLiu <44805397+JamieJLiu@users.noreply.github.com>
2018-12-18 17:20:54 +0800

    组件通信之子父通信
    显示余额

--- js/main.js
+++ js/main.js
@@ -1,21 +1,32 @@
-Vue.component('like',{
-    template: '#like-component-tpl',
-    data: function() {
-        return {
-            like_count:10,
-            liked: false,
-        }
-
-    },
+Vue.component('balance',{
+   template:`
+   <div>
+     <show @show-balance="show_balance"></show>
+     <div v-if="show">
+        您的余额：￥100000
+     </div>
+   </div>
+   `,
+   methods: {
+       show_balance: function(data) {
+           this.show = true;
+           console.log('data:', data);
+       }
+   },
+   data: function() {
+       return {
+           show: false,
+           
+       }
+   }
+});
+Vue.component('show',{
+    template:'<button @click="on_click()">显示余额</button>',
     methods:{
-        toggle_like: function(){
-            if (!this.liked)
-                this.like_count++;
-            else
-                this.like_count--;
-            this.liked = !this.liked;
+        on_click(){
+            this.$emit('show-balance',{a: 1, b: 2});
         }
-     }
+    }
 })
 
 new Vue({
